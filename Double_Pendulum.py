cd3e8737d9512744fa1f504a26ef5c45b8fa10ca
Cedric Ho <cedricjunjun@gmail.com>
2025-12-25 01:52:01 -0800

    Defined some variables

---
+++ Double_Pendulum.py
@@ -0,0 +1,12 @@
+# Imports for math
+import numpy
+import sympy
+
+# Define the variables and funcions used
+m1, m2, L1, L2, t, g = sympy.symbols('m1 m2 L1 L2 t g')
+
+m1 = 1
+m2 = 1
+L1 = 1
+L2 = 1
+g = 9.8
